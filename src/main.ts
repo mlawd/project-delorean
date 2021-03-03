98edfd5fde7e63da26108b525523ef1a6aae03e5
Matt Law <me@mattlaw.dev>
2021-03-03 22:19:34 +0000

    PWA

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {
+    name: "world",
+  },
 });
 
 export default app;
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
